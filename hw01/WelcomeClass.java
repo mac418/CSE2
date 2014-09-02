92e1947a468ebc42903341833963828c7e95afa4
mac418 <mac418@lehigh.edu>
2014-09-02 19:28:23 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+ 
+/////////////////////////////////////////
+//Maria Castro
+//CSE2 Hw01
+//WelcomeClass
+
+
+public class WelcomeClass{
+
+    public static void main (String[] args) {
+    
+//statement output printing
+
+        System.out.println("    -----------    ");
+        System.out.println("    | WELCOME |    ");
+        System.out.println("    -----------    ");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-M--A--C--4--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("I was born in El Salvador,");
+        System.out.println("but moved to Nicaragua two years later.");
+        System.out.println("I'm an only child with three dogs and a cat.");
+
+
+}
+    }
